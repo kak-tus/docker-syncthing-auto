59b50c20067c9a8630df33e5492e4b18cc216661
Andrey Kuzmin <prepodam@mail.ru>
2016-12-08 01:03:10 +0300

    No warn in logs.
    If user/group not exists.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-deluser user
-delgroup user
+deluser user 2>/dev/null
+delgroup user 2>/dev/null
 addgroup -g $USER_GID user
 adduser -h /home/user -G user -D -u $USER_UID user
 
