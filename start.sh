7c8dad89e9e4ae1311f6e3d3b68a40c6ce2237c3
Andrey Kuzmin <prepodam@mail.ru>
2017-09-06 23:19:37 +0300

    Set time zone.

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+if [ "$SET_CONTAINER_TIMEZONE" = "true" ]; then
+    echo "$CONTAINER_TIMEZONE" > /etc/timezone \
+    && ln -sf "/usr/share/zoneinfo/$CONTAINER_TIMEZONE" /etc/localtime
+    echo "Container timezone set to: $CONTAINER_TIMEZONE"
+else
+    echo "Container timezone not modified"
+fi
+
 deluser user 2>/dev/null
 delgroup user 2>/dev/null
 addgroup -g $USER_GID user
