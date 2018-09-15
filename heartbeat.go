c27bd7f3c374c39da7785c44cf011df0bada60f1
Andrey Kuzmin <prepodam@mail.ru>
2018-09-15 16:59:13 +0300

    Fix delete old folders without items in list.
    We iterate thru list IPs but not thru folders. Fix this.

--- heartbeat.go
+++ heartbeat.go
@@ -9,22 +9,27 @@ import (
 	"github.com/hashicorp/consul/api"
 )
 
+var kv *api.KV
+var dtLimit time.Time
+var dt time.Time
+var srv string
+
 func main() {
 	client, err := api.NewClient(api.DefaultConfig())
 	if err != nil {
 		panic(err)
 	}
 
-	kv := client.KV()
+	kv = client.KV()
 
 	ipID := os.Getenv("SYNC_IP") + "-" + os.Getenv("SYNC_PORT")
-	dt := time.Now()
-	srv := os.Getenv("SYNC_SERVICE")
-	dtLimit := dt.Add(-time.Duration(12 * time.Hour))
+	dt = time.Now()
+	srv = os.Getenv("SYNC_SERVICE")
+	dtLimit = dt.Add(-time.Duration(12 * time.Hour))
 
 	timeKey := "service/syncthing-auto/" + srv + "/devices/" + ipID + "/time"
 
-	saveTime(kv, timeKey, dt)
+	saveTime(timeKey, dt)
 
 	list, _, err := kv.List("service/syncthing-auto/"+srv+"/devices/list", nil)
 	if err != nil {
@@ -35,39 +40,63 @@ func main() {
 		str := strings.Split(val.Key, "/")
 		ip := str[len(str)-1]
 
-		timeKey = "service/syncthing-auto/" + srv + "/devices/" + ip + "/time"
-		timeval, _, err := kv.Get(timeKey, nil)
-		if err != nil {
-			panic(err)
-		}
+		processItem(ip)
+	}
 
-		if timeval == nil {
-			saveTime(kv, timeKey, dt)
+	list, _, err = kv.List("service/syncthing-auto/"+srv+"/devices", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	ips := make(map[string]bool)
+
+	for _, val := range list {
+		str := strings.Split(val.Key, "/")
+		ip := str[4]
+
+		if ip == "list" || ips[ip] {
 			continue
 		}
 
-		dtOld, err := time.Parse(time.RFC3339, string(timeval.Value))
+		ips[ip] = true
+		processItem(ip)
+	}
+}
+
+func processItem(ip string) {
+	key := "service/syncthing-auto/" + srv + "/devices/" + ip + "/time"
+
+	timeval, _, err := kv.Get(key, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	if timeval == nil {
+		saveTime(key, dt)
+		return
+	}
+
+	dtOld, err := time.Parse(time.RFC3339, string(timeval.Value))
+	if err != nil {
+		panic(err)
+	}
+
+	if dtOld.Before(dtLimit) {
+		fmt.Println("Delete " + ip)
+
+		_, err = kv.Delete("service/syncthing-auto/"+srv+"/devices/list/"+ip, nil)
 		if err != nil {
-			panic(err)
+			println(err)
 		}
 
-		if dtOld.Before(dtLimit) {
-			fmt.Println("Delete " + ip)
-
-			_, err = kv.Delete("service/syncthing-auto/"+srv+"/devices/list/"+ip, nil)
-			if err != nil {
-				panic(err)
-			}
-
-			_, err = kv.DeleteTree("service/syncthing-auto/"+srv+"/devices/"+ip, nil)
-			if err != nil {
-				panic(err)
-			}
+		_, err = kv.DeleteTree("service/syncthing-auto/"+srv+"/devices/"+ip, nil)
+		if err != nil {
+			println(err)
 		}
 	}
 }
 
-func saveTime(kv *api.KV, key string, dt time.Time) {
+func saveTime(key string, dt time.Time) {
 	put := &api.KVPair{Key: key, Value: []byte(dt.Format(time.RFC3339))}
 	_, err := kv.Put(put, nil)
 	if err != nil {
